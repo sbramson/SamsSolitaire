0ae26891ce68fad2a9338ced1a1d42f025d00d6c
Sam Bramson <sambramson@gmail.com>
2018-09-16 02:55:02 +0100

    first thoughts on how to TDD from the beginning

---
+++ src/test/java/LegalBoard.java
@@ -0,0 +1,12 @@
+/*
+ * LegalBoard.java
+ * A JUnit test 
+ *
+ * so this could potentially call smaller tests that check the legality of smaller parts of the board
+ * i.e. individual free cells, individual foundation piles, individual tableu columns
+ * but even if they all confirmed that they had legal cards, a subset of the deck, limited to whatever
+ * bounds (such as 0 to 1 for free cells) and under whatever restrictions (alternating colors in tableu
+ * columns, or strict increasing order within a suit for foundation piles), they would not naturally pick
+ * up the global information that THIS test would need, namely to identify that all 52 playing cards are
+ * out there somewhere
+ */
